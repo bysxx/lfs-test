fb150a50d11d85e3d2341d8e6ccd3675d2811f2d
bysxx <0415white@gmail.com>
2024-06-10 20:26:48 +0900

    feat: update scene 1, 2

--- Assets/02Scripts/Handler/StageHandler.cs
+++ Assets/02Scripts/Handler/StageHandler.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class StageHandler : MonoBehaviour
 {
     [SerializeField] private Quest quest;
+    [SerializeField] private GameObject car;
 
     // Start is called before the first frame update
     void Start()
     {
+        Access.QuestM.OnQuestRegisteredHandler += (quest) => {
+            car.SetActive(true);
+            Debug.Log("Car is activated");
+        };
+
         Access.QuestM.OnQuestCompletedHandler += (quest) => {
             Access.SceneM.LoadScene("stage1-2", 3.0f);
         };
